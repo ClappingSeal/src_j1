7b3ce1943c90edd6a05612cda7c1d6cbd43a88f5
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2024-08-28 06:59:58 +0900

    Update drone_node1.py

--- drone_package/drone_package/drone_node1.py
+++ drone_package/drone_package/drone_node1.py
@@ -139,7 +139,7 @@ class DroneNode1(Node):
         else:
             env = APFEnvironment(current_position)
             next_position = current_position + np.array(
-                env.apf(goal=self.goal_position, obs_pos=self.other_drones_positions)) * self.force
+                env.apf(goal=self.goal_position, obs_info=self.other_drones_positions)) * self.force
             self.goto(next_position[0], next_position[1], 4)
 
     # takeoff function
