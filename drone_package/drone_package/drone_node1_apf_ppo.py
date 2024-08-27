084bafa61dad6adb5363986fac738dd0155489a1
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2024-08-28 05:31:01 +0900

    Update drone_node1_apf_ppo.py

--- drone_package/drone_package/drone_node1_apf_ppo.py
+++ drone_package/drone_package/drone_node1_apf_ppo.py
@@ -62,7 +62,7 @@ class DroneNode1(Node):
         self.force = 5
 
         model_path = '/home/qwer/ros2_study/src_j1/drone_package/drone_package/ppo_robot.zip'
-        self.model = TD3.load(model_path)
+        self.model = PPO.load(model_path)
 
     # for Publisher Node
     def publish_position(self):
