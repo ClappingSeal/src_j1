661afd2754e4fe06ec6b8f77197ead7bfe2873d0
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2024-08-28 18:07:46 +0900

    Update drone_node1.py

--- drone_package/drone_package/drone_node1.py
+++ drone_package/drone_package/drone_node1.py
@@ -59,8 +59,6 @@ class DroneNode1(Node):
         self.landing_threshold = 1.0
         self.force = 5
 
-        self.start_path_planning()
-
     # for Publisher Node
     def publish_position(self):
         location = self.vehicle.location.global_relative_frame
